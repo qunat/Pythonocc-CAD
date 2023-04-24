091918cc0ee3b9237c741728d729b25fc8c8bf77
loujiand <48970747+qunat@users.noreply.github.com>
2023-04-25 00:32:44 +0800

    Add something to sketch 2023/4/25

--- sketcher/sketcher.py
+++ sketcher/sketcher.py
@@ -30,6 +30,7 @@ from OCC.Core.Aspect import (Aspect_TOM_POINT,
 class SketchModule(object):
 	def __init__(self,parent=None):
 		self.parent=parent
+		self.sketch_type=None
 		self.select_shape_list = []
 		self.dragStartPosX = 0
 		self.dragStartPosY = 0
@@ -46,25 +47,50 @@ class SketchModule(object):
 		if self.select_windows.comboBox.currentText()=="XY平面":
 			self.parent.Displayshape_core.canva._display.View_Top()
 			self.parent.Displayshape_core.canva._display.FitAll()
-			self.do_draw()
+			
 		if self.select_windows.comboBox.currentText()=="YZ平面":
 			self.parent.Displayshape_core.canva._display.View_Right()
 			self.parent.Displayshape_core.canva._display.FitAll()
+			self.do_draw()
 		if self.select_windows.comboBox.currentText()=="XZ平面":
 			self.parent.Displayshape_core.canva._display.View_Front()
 			self.parent.Displayshape_core.canva._display.FitAll()
+			self.do_draw()
 	
 	def clicked_callback(self, shp, *kwargs):
 		try:
-			print(shp)
-			self.new_do_draw.draw_line(shp)# draw line
+			if self.sketch_type==4:
+				self.new_do_draw.draw_line(shp)# draw line
 		except Exception as e:
 			print(e)
 	def do_draw(self):
-		self.new_do_draw=sketch_line(self.parent)# draw line
+		if self.sketch_type==4:
+			self.new_do_draw=sketch_line(self.parent)# draw line
 		
 	def dynamics_drwa(self):
 		self.new_do_draw.dynamics_drwa_line()# draw line
+	def skecth_draw_profile(self):
+		self.sketch_type=1# profile draw
+		self.do_draw()
+	def skecth_draw_rectangle(self):
+		self.sketch_type=2# rectangle draw
+		self.do_draw()
+	def skecth_draw_circel(self):
+		self.sketch_type=3# circel draw
+		self.do_draw()
+	def skecth_draw_line(self):
+		self.sketch_type=4# line draw
+		self.do_draw()
+	def skecth_draw_arc(self):
+		self.sketch_type=5# arc draw
+		self.do_draw()
+	def skecth_draw_spline(self):
+		self.sketch_type=6# spline draw
+		self.do_draw()
+	def skecth_draw_point(self):
+		self.sketch_type=7# point draw
+		self.do_draw()
+		
 	
 	
 	
@@ -194,4 +220,85 @@ class sketch_line(object):
 		
 		
 			
+class sketch_circel(sketch_line):
+	def __init__(self,parent=None):
+		super(sketch_line, self).__init__()
+	
+	def draw_line(self, shape=None):
+		if self.parent.InteractiveOperate.InteractiveModule == "SKETCH":
+			(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.canva._display.View.ProjReferenceAxe(
+				self.parent.Displayshape_core.canva.dragStartPosX,
+				self.parent.Displayshape_core.canva.dragStartPosY)
+			
+			if shape != [] and isinstance(shape[0], TopoDS_Vertex):  # 捕捉端点
+				P = BRep_Tool.Pnt(shape[0])
+				x, y, z = P.X(), P.Y(), P.Z()
+			
+			if shape != [] and isinstance(shape[0], TopoDS_Wire):  # 捕捉线上任意点
+				direction = gp_Dir(vx, vy, vz)
+				line = gp_Lin(gp_Pnt(x, y, z), direction)
+				ais_line = Geom_Line(line)
+				edge_builder = BRepBuilderAPI_MakeEdge(line)
+				edge = edge_builder.Edge()
+				extrema = BRepExtrema.BRepExtrema_DistShapeShape(shape[0], edge)
+				nearest_point = extrema.PointOnShape1(1)
+				x, y, z = nearest_point.X(), nearest_point.Y(), nearest_point.Z()
+			
+			if len(self.point_count) == 0:
+				self.draw_point(x, y, z)
+				self.point = (x, y, z)
+				self.point_count.append(self.point)
+				self.show_line_dict[self.line_id] = None
+				self.parent.Displayshape_core.canva.mouse_move_Signal.trigger.connect(self.dynamics_drwa_line)
+			
+			
+			elif len(self.point_count) >= 1:
+				self.InteractiveModule = None
+				self.draw_point(x, y, z)  # end point
+				aSegment = GC_MakeSegment(
+					gp_Pnt(self.point_count[-1][0], self.point_count[-1][1], self.point_count[-1][2]),
+					gp_Pnt(x, y, z))
+				anEdge = BRepBuilderAPI_MakeEdge(aSegment.Value())
+				aWire = BRepBuilderAPI_MakeWire(anEdge.Edge()).Shape()
+				self.show_line_dict[self.line_id][0].SetShape(aWire)  # 将已经显示的零件设置成另外一个新零件
+				self.parent.Displayshape_core.canva._display.Context.Redisplay(self.show_line_dict[self.line_id][0],
+																			   True,
+																			   False)  # 重新计算更新已经显示的物体
+				self.line_id += 1
+				self.point_count.clear()
+	
+	def dynamics_drwa_line(self):
+		_dragStartPosY = self.parent.Displayshape_core.canva.dragStartPosY
+		_dragStartPosX = self.parent.Displayshape_core.canva.dragStartPosX
+		if self.dragStartPosY != _dragStartPosY or self.dragStartPosX != _dragStartPosX:
+			
+			(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.canva._display.View.ProjReferenceAxe(
+				_dragStartPosX,
+				_dragStartPosY)
+			# print(x, y, z)
+			try:
+				x0 = self.point[0]
+				y0 = self.point[1]
+				z0 = self.point[2]
+				aSegment = GC_MakeSegment(gp_Pnt(x0, y0, z0), gp_Pnt(x, y, z))
+				anEdge = BRepBuilderAPI_MakeEdge(aSegment.Value())
+				aWire = BRepBuilderAPI_MakeWire(anEdge.Edge()).Shape()
+				
+				if self.show_line_dict[self.line_id] == None:
+					self.show_line_dict[self.line_id] = AIS_Shape(aWire)
+					self.show_line_dict[self.line_id] = self.parent.Displayshape_core.canva._display.DisplayShape(aWire,
+																												  True,
+																												  False)  # 重新计算更新已经显示的物体
+				else:
+					self.show_line_dict[self.line_id][0].SetShape(aWire)  # 将已经显示的零件设置成另外一个新零件
+				self.parent.Displayshape_core.canva._display.Context.Redisplay(self.show_line_dict[self.line_id][0],
+																			   True,
+																			   False)  # 重新计算更新已经显示的物体
+				self.parent.Displayshape_core.canva._display.Context.UpdateCurrentViewer()
+			
+			
+			except Exception as e:
+				pass
 		
+		self.dragStartPosX = _dragStartPosX
+		self.dragStartPosY = _dragStartPosY
