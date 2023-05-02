265ed4a9e04d7496499b3a8c78e64a7659f8eb18
loujiand <304817449@qq.com>
2023-05-02 14:13:21 +0800

    trim_line out of trim

--- sketcher/sketcher_trim-1.py
+++ sketcher/sketcher_trim-1.py
@@ -60,86 +60,79 @@ class sketch_trim(object):
 
 	def trim(self,shape=None):
 		self.get_all_sketch_show()
-		trim_point_min_1 = None
+		trim_point_min_1=None
 		trim_point_min_2 = None
-		distan_min_1 = 0
+		distan_min_1=0
 		distan_min_2 = 0
-		distance = 0
-		trim_shape = None
+		distance=0
+		trim_shape=None
 		for key in self.sketch_show_dict.keys():
 			try:
 				# 创建线段修剪器对象
-				key = int(key.replace("line", ""))
-				shp = self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key]
+				key=int(key.replace("line",""))
+				shp=self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key]
 				if shp.Shape().IsSame(shape[0]):
-					trim_shape_key = key
+					trim_shape_key=key
 					continue
 				extrema = BRepExtrema.BRepExtrema_DistShapeShape(shp.Shape(), shape[0])
 				nearest_point_1 = extrema.PointOnShape1(1)
-				nearest_point_2 = extrema.PointOnShape2(1)
+				nearest_point_2=extrema.PointOnShape2(1)
 
-				if math.floor(nearest_point_1.Distance(nearest_point_2)) != 0:
+				if math.floor(nearest_point_1.Distance(nearest_point_2))!=0:
 					continue
 				x, y, z = nearest_point_1.X(), nearest_point_1.Y(), nearest_point_1.Z()
-				trim_point = gp_Pnt(x, y, z)
+				trim_point=gp_Pnt(x,y,z)
+
 
 				(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.canva._display.View.ProjReferenceAxe(
 					self.parent.Displayshape_core.canva.dragStartPosX,
 					self.parent.Displayshape_core.canva.dragStartPosY)
 				mouse_point = gp_Pnt(x, y, z)
-				distance = trim_point.Distance(mouse_point)
+				distance= trim_point.Distance(mouse_point)
 
-				if distan_min_1 == 0:
-					distan_min_1 = distance
-					trim_point_min_1 = trim_point
+				if distan_min_1==0:
+					distan_min_1=distance
+					trim_point_min_1=trim_point
 
 				elif distance < distan_min_1:
 					trim_point_min_2 = trim_point_min_1
-					distan_min_2 = distan_min_1
+					distan_min_2=distan_min_1
 					distan_min_1 = distance
 					trim_point_min_1 = trim_point
 				else:
-					if distance < distan_min_2 or distan_min_2 == 0:
-						distan_min_2 = distance
-						trim_point_min_2 = trim_point
+					if distance < distan_min_2 or distan_min_2==0:
+						distan_min_2=distance
+						trim_point_min_2=trim_point
 
-				if trim_point_min_2==None:
-					trim_point_min_2=trim_point_min_1
+
 			except Exception as e:
-				print(e)
 				pass
-				#--------------------------------------------------------------------------------------line_trim
-		self.trim_line(shape,trim_point_min_1,trim_point_min_2,distan_min_1,distan_min_2,trim_shape_key)
+		distance=trim_point_min_1.Distance(trim_point_min_2)
 
-
-
-
-	def trim_line(self,shape,trim_point_min_1,trim_point_min_2,distan_min_1,distan_min_2,trim_shape_key):
-		#To do trim line
-		distance = trim_point_min_1.Distance(trim_point_min_2)
 		point = self.parent.Displayshape_core.canva._display.DisplayShape(trim_point_min_1, color="YELLOW",
 																		  update=False)  # 重新计算更新已经显示的物
 		self.parent.Displayshape_core.canva._display.Repaint()
 		point = self.parent.Displayshape_core.canva._display.DisplayShape(trim_point_min_2, color="YELLOW",
 																		  update=False)  # 重新计算更新已经显示的物
 		self.parent.Displayshape_core.canva._display.Repaint()
-		# --------------------------------------------------------------------------------------------------
+		#--------------------------------------------------------------------------------------------------
 		explorer = BRepTools_WireExplorer(shape[0])
 		startVertex = explorer.CurrentVertex()
 		explorer.Next()
-
+		
 		endVertex = explorer.CurrentVertex()
 		explorer.Next()
 		P1 = BRep_Tool.Pnt(startVertex)  # 转换为pnt数据
 		P2 = BRep_Tool.Pnt(endVertex)  # 转换为pnt数据
-
+		
+		
 		(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.canva._display.View.ProjReferenceAxe(
 			self.parent.Displayshape_core.canva.dragStartPosX,
 			self.parent.Displayshape_core.canva.dragStartPosY)
 		mouse_point = gp_Pnt(x, y, z)
-
-		if math.floor(distan_min_1 + distan_min_2) == math.floor(distance) and distance!=0:
-
+		
+		if math.floor(distan_min_1+distan_min_2)==math.floor(distance):
+			
 			distance1 = P1.Distance(trim_point_min_1)
 			distance2 = P1.Distance(trim_point_min_2)
 			if distance1 < distance2:
@@ -152,28 +145,27 @@ class sketch_trim(object):
 			aSegment = GC_MakeSegment(trim_point_min_1, end_point_1)
 			anEdge = BRepBuilderAPI_MakeEdge(aSegment.Value())
 			aWire = BRepBuilderAPI_MakeWire(anEdge.Edge()).Shape()
-			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetShape(
-				aWire)  # 将已经显示的零件设置成另外一个新零件
+			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetShape(aWire)  # 将已经显示的零件设置成另外一个新零件
 			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetWidth(self.width)
-			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetColor(
-				Quantity_Color(Quantity_NOC_BLACK))
-			self.parent.Displayshape_core.canva._display.Context.Redisplay(
-				self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key], True,
-				False)  # 重新计算更新已经显示的物体
+			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetColor(Quantity_Color(Quantity_NOC_BLACK))
+			self.parent.Displayshape_core.canva._display.Context.Redisplay(self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key], True, False)  # 重新计算更新已经显示的物体
+			
 
 			aSegment = GC_MakeSegment(trim_point_min_2, end_point_2)
 			anEdge = BRepBuilderAPI_MakeEdge(aSegment.Value())
 			aWire = BRepBuilderAPI_MakeWire(anEdge.Edge()).Shape()
 
 			key = self.parent.Sketcher.new_do_draw_dict["line"].line_id
-			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key] = AIS_Shape(aWire)
+			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key]=AIS_Shape(aWire)
 			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key].SetWidth(self.width)
 			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key].SetColor(Quantity_Color(self.color))
-			self.parent.Displayshape_core.canva._display.Context.Display(
-				self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key], True)  # 重新计算更新已经显示的物体
-			self.parent.Sketcher.new_do_draw_dict["line"].line_id = key + 1
+			self.parent.Displayshape_core.canva._display.Context.Display(self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[key], True)  # 重新计算更新已经显示的物体
+			self.parent.Sketcher.new_do_draw_dict["line"].line_id=key+1
+			
+		
 
 		else:
+			
 			distance1 = P1.Distance(mouse_point)
 			distance2 = P2.Distance(mouse_point)
 			if distance1 > distance2:
@@ -183,17 +175,12 @@ class sketch_trim(object):
 			aSegment = GC_MakeSegment(trim_point_min_1, end_point)
 			anEdge = BRepBuilderAPI_MakeEdge(aSegment.Value())
 			aWire = BRepBuilderAPI_MakeWire(anEdge.Edge()).Shape()
-			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetShape(
-				aWire)  # 将已经显示的零件设置成另外一个新零件
+			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetShape(aWire)  # 将已经显示的零件设置成另外一个新零件
 			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetWidth(self.width)
-			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetColor(
-				Quantity_Color(self.color))
-			self.parent.Displayshape_core.canva._display.Context.Redisplay(
-				self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key], True,
-				False)  # 重新计算更新已经显示的物体
+			self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key].SetColor(Quantity_Color(self.color))
+			self.parent.Displayshape_core.canva._display.Context.Redisplay(self.parent.Sketcher.new_do_draw_dict["line"].show_line_dict[trim_shape_key], True, False)  # 重新计算更新已经显示的物体
 
-
-def draw_point(self,x,y,z,point_type=None,color=None):
+	def draw_point(self,x,y,z,point_type=None,color=None):
 		ALL_ASPECTS = [Aspect_TOM_POINT,
 					   Aspect_TOM_PLUS,
 					   Aspect_TOM_STAR,
