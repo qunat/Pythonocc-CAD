e62c56a4ba7ed3b9e9ad223be16e05965a3428b5
loujiand <304817449@qq.com>
2023-05-15 05:44:36 +0800

    complete capure point

--- module/InteractiveModule.py
+++ module/InteractiveModule.py
@@ -4,10 +4,13 @@ import time
 
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire
 from OCC.Core.GC import GC_MakeSegment
+from OCC.Core.Graphic3d import Graphic3d_Group, Graphic3d_MarkerImage, Graphic3d_AspectMarker3d
+from OCC.Core.Prs3d import Prs3d_PointAspect
 from OCC.Core.Quantity import Quantity_Color
 from OCC.Core.gp import gp_Pnt
 from OCC.Core.AIS import AIS_Shape
 from OCC.Core.Quantity import Quantity_Color, Quantity_TOC_RGB
+from OCC.Core.Aspect import Aspect_TOM_PLUS,Aspect_TOM_O
 
 class InteractiveOperate(object):
 	def __init__(self,parent=None):
@@ -18,22 +21,32 @@ class InteractiveOperate(object):
 		self.dragStartPosY = 0
 		self.aisline=None
 		self.point_count=[]
-		
+
 	def Setting(self):
 		HighlightStyle = self.parent.Displayshape_core.canva._display.Context.HighlightStyle()
 		HighlightStyle.SetColor(Quantity_Color(0 / 255, 128 / 255, 0 / 255, Quantity_TOC_RGB))
 		SelectionStyle = self.parent.Displayshape_core.canva._display.Context.SelectionStyle()
+		HighlightStyle.SetMethod(Aspect_TOM_O)# 颜色显示方式
 		# SelectionStyle.SetMethod(Aspect_TOHM_COLOR)# 颜色显示方式
 		SelectionStyle.SetColor(Quantity_Color(0 / 255, 0 / 255, 0 / 255, Quantity_TOC_RGB))  # 设置选择后颜色
 		# SelectionStyle.t_select_style->SetDisplayMode(1)#整体高亮
 		# t_select_style->SetTransparency(0.1)
 
+		#高亮点的样式设置
+		#aspectMarker=Graphic3d_AspectMarker3d(Aspect_TOM_PLUS)
+		#aspectMarker.SetColor(Quantity_Color(1.0, 0.0, 0.0, Quantity_TOC_RGB))
+		#asp = Prs3d_PointAspect(Aspect_TOM_PLUS, Quantity_Color(1.0, 0.0, 0.0, Quantity_TOC_RGB), 4)
+
+
 		self.parent.Displayshape_core.canva._display.Context.SetSelectionStyle(HighlightStyle)
 		self.parent.Displayshape_core.canva._display.Context.SetSelectionStyle(SelectionStyle)
+		#self.parent.Displayshape_core.canva._display.Context.SetHighlightStyle(aspectMarker)
 
-				
-		
+		#myGroup = Graphic3d_Group(self.parent.Displayshape_core.canva._display.GetView())
+		#myMarker = Graphic3d_MarkerImage(Aspect_TOM_O, Quantity_Color(c), 8, 1)
+		#myGroup.Marker(myMarker)
 
-	
+
+
 
 
--- sketcher/sketcher_line.py
+++ sketcher/sketcher_line.py
@@ -5,10 +5,11 @@ from OCC.Core import BRepExtrema
 from OCC.Core.BRep import BRep_Tool
 from OCC.Core.GCE2d import GCE2d_MakeLine
 from OCC.Core.Geom import Geom_CartesianPoint, Geom_Line
+from OCC.Core.Graphic3d import Graphic3d_AspectMarker3d
 from OCC.Core.Prs3d import Prs3d_PointAspect
 from OCC.Core.Quantity import Quantity_Color
 from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.TopoDS import TopoDS_Vertex, TopoDS_Wire, TopoDS_Shape
+from OCC.Core.TopoDS import TopoDS_Vertex, TopoDS_Wire, TopoDS_Shape, TopoDS_Edge
 from OCC.Core.Quantity import Quantity_Color, Quantity_TOC_RGB
 from PyQt5.QtGui import QCursor, QPixmap
 
@@ -40,8 +41,10 @@ class Brep_line(object):
 	def __init__(self,parent=None,point1=[],point2=[]):
 		self.parent=parent
 		self.edge_point_list=[None,None]
-		self.middle_poind_list=[]
+		self.middle_poind_list=[None]
 		self.ais_shape=None
+		self.capture_point_list=[None,None,None]
+		self.capture_any_point_list = [None]
 		self.create_line(point1,point2)
 
 
@@ -58,7 +61,6 @@ class Brep_line(object):
 		self.ais_shape=AIS_Shape(line)
 		self.ais_shape.SetColor(Quantity_Color(self.parent.color))
 		self.ais_shape.SetWidth(self.parent.width)
-
 		self.edge_point_list[0]=self.create_end_point(p1)
 		self.edge_point_list[1] = self.create_end_point(p2)
 		self.display_line()
@@ -76,6 +78,34 @@ class Brep_line(object):
 		ais_point.SetAttributes(drawer)
 		return ais_point
 
+	def create_capture_point(self,*args):
+		if len(args)==1:
+			x,y,z=args[0]
+			point_type = Aspect_TOM_O_POINT
+			p = Geom_CartesianPoint(gp_Pnt(x, y, z))
+			color = Quantity_Color(1, 1, 1, Quantity_TOC_RGB)
+			ais_point = AIS_Point(p)
+			drawer = ais_point.Attributes()
+			asp = Prs3d_PointAspect(point_type, color, 4)
+			drawer.SetPointAspect(asp)
+			ais_point.SetAttributes(drawer)
+			return ais_point
+		elif len(args)==2:
+			x0, y0, z0 = args[0]
+			x1, y1, z1 = args[1]
+			x = (x0 + x1) / 2
+			y = (y0 + y1) / 2
+			z = (z0 + z1) / 2
+			point_type = Aspect_TOM_O_POINT
+			p = Geom_CartesianPoint(gp_Pnt(x, y, z))
+			color = Quantity_Color(1, 1, 1, Quantity_TOC_RGB)
+			ais_point = AIS_Point(p)
+			drawer = ais_point.Attributes()
+			asp = Prs3d_PointAspect(point_type, color, 4)
+			drawer.SetPointAspect(asp)
+			ais_point.SetAttributes(drawer)
+			return ais_point
+
 	def set_ais_shape(self,p1,p2):
 		x0, y0, z0 = p1
 		x1, y1, z1 = p2
@@ -92,21 +122,55 @@ class Brep_line(object):
 		self.parent.parent.Displayshape_core.canva._display.Context.Remove(self.edge_point_list[1], True)
 		self.edge_point_list[0] = self.create_end_point(p1)
 		self.edge_point_list[1] = self.create_end_point(p2)
+		self.capture_point_list[0]=self.create_capture_point(p1)
+		self.capture_point_list[1] = self.create_capture_point(p1,p2)
+		self.capture_point_list[2] = self.create_capture_point(p2)
+
 		self.redisplay()
+	def set_capture_any_point(self,x,y,z):
+		p1=[x,y,z]
+		self.capture_any_point_list[0] = self.create_capture_point(p1)
 
 	def display_line(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.ais_shape,False)#显示的物体
+
+
+	def redisplay(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Redisplay(self.ais_shape, True,False) #重新计算更新已经显示的物体
+		self.parent.parent.Displayshape_core.canva._display.Context.UpdateCurrentViewer()
+
+	def display_all(self):
 		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.ais_shape,False)#显示的物体
 		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.edge_point_list[0], False)  # 显示的物体
 		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.edge_point_list[1], False)  # 显示的物体
 
-	def redisplay(self):
+	def redisplay_all(self):
 		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.edge_point_list[0], False)  # 显示的物体
 		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.edge_point_list[1], False)  # 显示的物体
 		self.parent.parent.Displayshape_core.canva._display.Context.Redisplay(self.ais_shape, True,False) #重新计算更新已经显示的物体
 		self.parent.parent.Displayshape_core.canva._display.Context.UpdateCurrentViewer()
+	def display_capture_point(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.capture_point_list[0], False)  # 显示的物体
+		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.capture_point_list[1], False)  # 显示的物体
+		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.capture_point_list[2], False)  # 显示的物体
+
+	def remove_capture_point(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Remove(self.capture_point_list[0], True)  #移除捕捉的任意点
+		self.parent.parent.Displayshape_core.canva._display.Context.Remove(self.capture_point_list[1], True)  #移除捕捉的任意点
+		self.parent.parent.Displayshape_core.canva._display.Context.Remove(self.capture_point_list[2], True)  #移除捕捉的任意点
 
 
+	def display_capture_any_point(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Display(self.capture_any_point_list[0],
+																			False)  # 显示的物体
 
+	def remove_capture_any_point(self):
+		self.parent.parent.Displayshape_core.canva._display.Context.Remove(self.capture_any_point_list[0], False)  # 移除
+
+	def get_end_point_pnt(self,point):
+		Vertex0 = self.edge_point_list[point].Vertex()
+		pnt = BRep_Tool.Pnt(Vertex0)
+		return pnt
 
 
 
@@ -134,15 +198,13 @@ class sketch_line(object):
 
 	def draw_line(self,shape=None):
 			if self.parent.InteractiveOperate.InteractiveModule == "SKETCH":
+				print(shape)
 				(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.ProjReferenceAxe()
-
-				
 				if shape!=[] and isinstance(shape[0],TopoDS_Vertex) :#捕捉端点
 					P = BRep_Tool.Pnt(shape[0])
 					x,y,z=P.X(),P.Y(),P.Z()
-					print("enter1")
 					
-				if shape!=[] and isinstance(shape[0],TopoDS_Wire) :#捕捉线上任意点
+				if shape!=[] and isinstance(shape[0],TopoDS_Edge) :#捕捉线上任意点
 					direction = gp_Dir(vx, vy, vz)
 					line = gp_Lin(gp_Pnt(x, y, z), direction)
 					ais_line = Geom_Line(line)
@@ -151,9 +213,11 @@ class sketch_line(object):
 					extrema = BRepExtrema.BRepExtrema_DistShapeShape(shape[0], edge)
 					nearest_point = extrema.PointOnShape1(1)
 					x, y, z = nearest_point.X(), nearest_point.Y(), nearest_point.Z()
-				if shape==[]:
+
+				if shape==[]:#捕捉生成的端点
 					shape_id = 0
 					Distance = 0
+					print("enter this ")
 					_dragStartPosY = self.parent.Displayshape_core.canva.dragStartPosY
 					_dragStartPosX = self.parent.Displayshape_core.canva.dragStartPosX
 					(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.ProjReferenceAxe()
@@ -170,36 +234,36 @@ class sketch_line(object):
 							nearest_point2 = extrema.PointOnShape2(1)
 							if Distance > nearest_point1.Distance(nearest_point2) or Distance == 0:
 								Distance = nearest_point1.Distance(nearest_point2)
-								x, y, z = (nearest_point1.X()), (nearest_point1.Y()), (nearest_point1.Z())
 								shape_id = key
 							pass
 						except Exception as e:
 							print(e)
 							pass
-					# 捕捉生产端点
+
 					try:
-						if self.show_line_dict[shape_id].edge_point_list[0].Distance(gp_Pnt(x, y, z))<=6:
-							print("这就是端点")
-						if self.show_line_dict[shape_id].edge_point_list[1].Distance(gp_Pnt(x, y, z))<=6:
-							print("这就是端点")
+						point0=self.show_line_dict[shape_id].get_end_point_pnt(0)
+						point1 = self.show_line_dict[shape_id].get_end_point_pnt(1)
+						if point0.Distance(gp_Pnt(x, y, z))<=10:
+							x=point0.X()
+							y = point0.Y()
+							z = point0.Z()
+						elif point1.Distance(gp_Pnt(x, y, z))<=10:
+							x = point1.X()
+							y = point1.Y()
+							z = point1.Z()
+						else:
+							(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.ProjReferenceAxe()
 					except Exception as e:
+						print(e)
 						pass
 
-					if Distance > 10 or Distance == 0:
-						self.capture_point_None = 0
-					else:
-						self.capture_point_None = 1
-
-
-					
-				
 				if len(self.point_count) == 0:
 					#self.draw_point(x,y,z)
 					self.point = [x, y, z]
 					self.point_count.append(self.point)
 					self.show_line_dict[self.line_id] = None
 					self.parent.Displayshape_core.canva.mouse_move_Signal.trigger.connect(self.dynamics_drwa_line)
-					#self.parent.Displayshape_core.canva.mouse_move_Signal.trigger.connect(self.dynamics_draw_trance)
+					self.parent.Displayshape_core.canva.mouse_move_Signal.trigger.connect(self.dynamics_draw_trance)
 
 					
 					
@@ -213,10 +277,10 @@ class sketch_line(object):
 						capture_point=BRep_Tool.Pnt(self.capture_point.Vertex())
 						x,y,z=capture_point.X(),capture_point.Y(),capture_point.Z()
 
-					#print("capture_point",x,y,z)
 					p1=self.point
 					p2=[x,y,z]
 					self.show_line_dict[self.line_id].set_ais_shape(p1,p2)
+					self.show_line_dict[self.line_id].redisplay_all()
 					self.line_id+=1
 					self.point_count.clear()
 
@@ -230,14 +294,13 @@ class sketch_line(object):
 		(x, y, z, vx, vy, vz) = self.parent.Displayshape_core.ProjReferenceAxe()
 		direction = gp_Dir(vx, vy, vz)
 		line = gp_Lin(gp_Pnt(x, y, z), direction)
-		#ais_line = Geom_Line(line)
 		edge_builder = BRepBuilderAPI_MakeEdge(line)
 		edge = edge_builder.Edge()
 		for key in self.show_line_dict.keys():
 			try:
 				if key==self.line_id  and len(self.point_count) >= 1:
 					continue
-				extrema = BRepExtrema.BRepExtrema_DistShapeShape(self.show_line_dict[key].Shape(), edge)
+				extrema = BRepExtrema.BRepExtrema_DistShapeShape(self.show_line_dict[key].ais_shape.Shape(), edge)
 				nearest_point1 = extrema.PointOnShape1(1)
 				nearest_point2 = extrema.PointOnShape2(1)
 				if Distance>nearest_point1.Distance(nearest_point2) or Distance==0:
@@ -248,51 +311,29 @@ class sketch_line(object):
 			except Exception as e:
 				print(e)
 				pass
-		#捕捉生产端点和中点
+		#捕捉生产端点和中点,任意点
 		if Distance>10 or Distance==0:
 			self.capture_point_None=0
+			try:
+				self.show_line_dict[shape_id].remove_capture_point()
+				self.show_line_dict[shape_id].remove_capture_any_point()
+				self.parent.Displayshape_core.canva._display.Repaint()
+			except:
+				pass
 		else:
-			self.capture_point_None = 1
-			print(self.show_line_dict[shape_id].Shape())
-			edge=self.show_line_dict[shape_id].Shape()
-			vertex_explorer = TopExp_Explorer(edge, TopAbs_VERTEX)
-			v1 = TopoDS_Vertex(vertex_explorer.Current())
-			vertex_explorer.Next()
-			v2 = TopoDS_Vertex(vertex_explorer.Current())
-			p1=BRep_Tool.Pnt(v1)
-			p2 = BRep_Tool.Pnt(v2)
-			self.draw_point(p1.X(),p1.Y(),p1.Z())
-			self.draw_point(p2.X(), p2.Y(), p2.Z())
-
-
+			self.show_line_dict[shape_id].remove_capture_any_point()
+			self.show_line_dict[shape_id].set_capture_any_point(x, y, z)
+			self.show_line_dict[shape_id].display_capture_any_point()
+			self.show_line_dict[shape_id].display_capture_point()
+			self.parent.Displayshape_core.canva._display.Repaint()
+			pass
 
 		if len(self.show_line_dict.keys())!=0 and 0<Distance<=10:
-			#pixmap = QPixmap('./icons/cursorlocation_2l-transformed.png')
-			# 2. 将光标对象传入鼠标对象中
-			#cursor = QCursor(pixmap)
-			# 3. 设置控件的光标
-			#self.parent.setCursor(cursor)
-			p = Geom_CartesianPoint(gp_Pnt(x, y, z))
-			color = Quantity_Color(0, 0, 0, Quantity_TOC_RGB)
+
 			if self.capture_point_list==[]:
 				pass
-				self.capture_point= AIS_Point(p)
-				self.parent.Displayshape_core.canva._display.Context.Display(self.capture_point,False)  # 重新计算更新已经显示的物
-				self.capture_point_list.append(self.capture_point)
 			elif len(self.capture_point_list)==1:
 				pass
-				self.parent.Displayshape_core.canva._display.Context.Remove(self.capture_point,True)
-				self.capture_point = AIS_Point(p)
-				self.capture_point.Redisplay()
-				self.parent.Displayshape_core.canva._display.Context.Display(self.capture_point,False)  # 重新计算更新已经显示的物体
-				self.parent.Displayshape_core.canva._display.Repaint()
-		else:
-			self.parent.unsetCursor()
-
-
-
-
-
 
 	def dynamics_drwa_line(self):
 			_dragStartPosY = self.parent.Displayshape_core.canva.dragStartPosY
@@ -316,7 +357,6 @@ class sketch_line(object):
 			
 			self.dragStartPosX = _dragStartPosX
 			self.dragStartPosY = _dragStartPosY
-	
 
 	def draw_point(self,x,y,z,point_type=None,color=None):
 
@@ -343,6 +383,8 @@ class sketch_line(object):
 			asp = Prs3d_PointAspect(point_type, color, 4)
 			drawer.SetPointAspect(asp)
 			ais_point.SetAttributes(drawer)
+			#myMarker =Graphic3d_AspectMarker3d(Aspect_TOM_CIRCLE,Quantity_Color(1.0, 0.0, 0.0, Quantity_TOC_RGB),10.0 )
+
 			point = self.parent.Displayshape_core.canva._display.Context.Display(ais_point,
 																				 False)  # 重新计算更新已经显示的物
 		else:
